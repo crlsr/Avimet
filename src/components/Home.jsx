962ddc6413de464bdc2cdd7680d4853f5db09a70
mimigarci <michelle.garcia@correo.unimet.edu.ve>
2025-03-04 10:47:18 -0400

    test

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
 const Home = () => {
     return (
+
         <div>
             <h1>Home</h1>
+            
+            <header>
+
+            
+            </header>
+
+
+
         </div>
     )
 }
