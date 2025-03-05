258a13643ec1a8f95a0e651172fc00998671e0ca
Gabriele Colarusso <168133113+gabrielecolarusso@users.noreply.github.com>
2025-03-04 22:01:48 -0400

    Ya el carrusel funciona, todo en home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
+import "./LoginSignup.css";
+import ProductCarousel from "../components/ProductCarousel/ProductCarousel";
+import TarjetaDestinos, { destinosData } from "../components/TarjetaDestinos/TarjetaDestinos";
+import TarjetaMisionVision from "../components/TarjetaMisionVision/TarjetaMisionVision";
+
 const Home = () => {
     return (
-        <div>
+        <>
+        <div className="container">
             <h1>Bienvenido</h1>
+            <ProductCarousel/>
         </div>
+        {/* Sección de tarjetas de información */}
+      <div className="tarjetas-container">
+        {destinosData.map((destino, index) => (
+          <TarjetaDestinos
+            key={index}
+            imagen={destino.imagen}
+            titulo={destino.titulo}
+            descripcion={destino.descripcion}
+            colorClase={destino.colorClase} // Pasa la clase de color aquí
+          />
+        ))}
+      </div>
+      <div>
+        <TarjetaMisionVision />
+      </div>
+        </>
+
     )
 }
 
