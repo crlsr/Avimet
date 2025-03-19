2cb6953a1cb77a212ab9e3ae2337ccf300133340
RojasA2005 <andresrojaslarocca16@gmail.com>
2025-03-19 13:54:07 -0400

    email
    Función para mandar correo al admin

--- src/components/Postulacion/Postulacion.jsx
+++ src/components/Postulacion/Postulacion.jsx
@@ -4,18 +4,51 @@ import './Postulacion.module.css'; // Importamos los estilos
 import { collection, addDoc } from "firebase/firestore";
 import appFirebase from "../../../credenciales";
 import { getFirestore } from "firebase/firestore";
+import emailjs from 'emailjs-com';
 
 const db = getFirestore(appFirebase);
 
-const Postulacion = (idPersona, slug_destino, NombreDest, NombrePer) => {
+const Postulacion = (NombreDest, NombrePer) => {
+
+  const serviceID = 'wejfverijvfwoeirjfjrkqvw';
+  const templateID = 'template_57a9632';
+  const userID = 'EpWodUNXN2HZHuy6Y';
+
+  const templateParams = {
+    name: name,
+    email: email,
+    message: "El usuario " + NombrePer + " desea ser el guía designado de la ruta " + NombreDest,
+    time: new Date().toLocaleString()
+  };
+
   // Función que se ejecuta al hacer clic en el botón
   const handleClick = async () => {
-    const docRef = await addDoc(collection(db, 'postulaciones'), {
-        idusuario: idPersona, // Datos del documento
-        slug: slug_destino,
-        nombredest: NombreDest,
-        nombrePer: NombrePer
-      });
+    const itemsRef = collection(db, "users");
+      let q;
+      q = query(itemsRef, where("userType", "is", "admin"));
+    
+        try {
+          const querySnapshot = await getDocs(q);
+          const emails = []; // Array para almacenar los nombres
+    
+          querySnapshot.forEach((doc) => {
+            emails.push(doc.data().email); // Asegúrate de que el campo "nombre" exista
+            templateParams.email = email;
+            emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_USER_ID')
+            .then((result) => {
+                console.log(result.text);
+                alert('Correo enviado con éxito!');
+            }, (error) => {
+                console.log(error.text);
+                alert('Hubo un error al enviar el correo.');
+            });
+          });
+        }  catch (error) {
+          console.error("Error al obtener documentos: ", error);
+        }
+
+      
+     
   };
 
   return (
